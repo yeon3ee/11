eff82d4b56e8243b09fe92005ee18098916eb865
yeon3ee <2310538@sookmyung.ac.kr>
2023-11-16 15:35:30 +0900

    re02

--- main.c
+++ main.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
-	int i=10;
-	char c='a';
-
-   
-    int* iptr = &i;
-    char* cptr = &c;
-    int* iptr2 = iptr;
-
-   	printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
-	printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr));
-	printf("iptr2 : %p, %i\n", iptr2, *iptr2);
-    return 0;
+void main(void) {
+	int i = 4; 
+	int* pi = &i;
+	char* pc = &i;
+	printf("%i,%i,%i\n",i,*pi,*pc);
 }
