ec3a28793f5d10135273c47746d3ba7eea80628c
yeon3ee <2310538@sookmyung.ac.kr>
2023-11-16 15:43:08 +0900

    re03-2

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap (int x, int y) {
+void swap (int *x, int *y) {
 	int temp;
-	temp = x;
-	x = y;
-	y = temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
 }
 void main(int argc, char *argv[]) {
 	int a = 3;
 	int b = 5;
-	swap(a, b);
+	swap(&a, &b);
 	printf("a:%i, b:%i\n", a, b);
 }
