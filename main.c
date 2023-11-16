806db9ec013d8c03e57cdea51f7cb70653a39c11
yeon3ee <2310538@sookmyung.ac.kr>
2023-11-16 15:20:46 +0900

    03-1

--- main.c
+++ main.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
-	int i=10;
-	char c='a';
-    int *iptr;
-    iptr = &i;
-    char *cptr;
-    cptr = &c;
-    int *iptr2; 
-    iptr2 = iptr; 
-    printf("i: %d, iptr: %p\n", i, (void*)iptr);
-    printf("c: %c, cptr: %p\n", c, (void*)cptr);
-    printf("iptr2: %p, value: %d\n", (void*)iptr2, *iptr2);
-
-    return 0;
+void swap (int x, int y) {
+	int temp;
+	temp = x;
+	x = y;
+	y = temp;
+}
+void main(void) {
+	int a = 3;
+	int b = 5;
+	swap(a, b);
+	printf("a:%i, b:%i\n", a, b);
 }
